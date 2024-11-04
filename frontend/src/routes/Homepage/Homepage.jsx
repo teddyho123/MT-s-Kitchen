b3b4339d957084c288043dc257b29849cef858db
miketsang-takhin <miketsang.takhin.us@gmail.com>
2024-11-04 10:14:50 -0600

    11-04-2024 Mike's Update

--- frontend/src/routes/Homepage/Homepage.jsx
+++ frontend/src/routes/Homepage/Homepage.jsx
@@ -8,6 +8,15 @@ function Homepage() {
       <main className="home-main">
         <div>
           <Navbar />
+
+          <div className="hero">
+            <div className="hero-content">
+              <h1>Share your perfect recipes to the world</h1>
+              <p>Food is a love language for many. Spread the love by sharing your recipes, or discover a world of flavors in our recipe library to share with those you cherish.</p>
+              <button className="btn"><a href="/recipes">Feel Lucky</a></button>
+            </div>
+          </div>
+
           <h1 className="title">Welcome to MT's Kitchen!</h1>
           <div className="sukiyaki">
             <img src={sukiyaki} alt="sukiyaki"/>
