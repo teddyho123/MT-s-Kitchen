27340a5a2e25acf143d485d4d3b13b6a29073855
miketsang-takhin <miketsang.takhin.us@gmail.com>
2024-11-07 12:11:21 -0600

    11-07-2024 Mike's Update (RecipeCard update)

--- frontend/src/routes/Recipes/Recipes.jsx
+++ frontend/src/routes/Recipes/Recipes.jsx
@@ -1,9 +1,8 @@
 import { useState, useEffect } from "react";
 import Footer from "../../components/Footer/Footer";
 import Navbar from "../../components/Navbar/Navbar";
-import RecipeLikeButton from "../RecipeDetail/RecipesLikeButton";
+import RecipeCard from "../../components/RecipeCard/RecipeCard.jsx";
 import "./Recipes.css";
-import RecipeCard from "../../components/RecipeCard/RecipeCard";
 
 function Recipes() {
   const [recipes, setRecipes] = useState([]);
@@ -39,13 +38,10 @@ function Recipes() {
         </div>
 
         <div className="card-grid">
-        {Array.isArray(recipes)
-          ? recipes.map((recipe) => (
-              <RecipeCard key={recipe.id} recipe={recipe} />
-            ))
-          : <h1>No Recipes available</h1>}
-      </div>
-
+          {Array.isArray(recipes) ? recipes.map((recipe) => (
+            <RecipeCard key={recipe.id} recipe={recipe} />
+          )) : <h1>No Recipes available</h1>}
+        </div>
         <Footer />
       </div>
     </>
